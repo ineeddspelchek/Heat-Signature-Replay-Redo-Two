40f9f9f7dae03cb4e0ec5a0c80b2175b7747b173
Henry Newton <74257157+ineeddspelchek@users.noreply.github.com>
2023-10-17 12:58:22 -0400

    even more accurate timing with better pause handling

--- Lambda_Deployment/lambda_function.py
+++ Lambda_Deployment/lambda_function.py
@@ -20,8 +20,8 @@ def lambda_handler(event, context):
     return edit(times, key, s3client)
 
 def edit(times, key, s3client): #create and edit raw footage from speed change timestamps
-    pauseOffset = -.04 #how much earlier to start pause to make sure its frames aren't included
-    unpauseOffset = .03 #how much later to end unpause to make sure its frames aren't included
+    pauseOffset = .05 #how much earlier to start pause to make sure its frames aren't included
+    unpauseOffset = .05 #how much later to end unpause to make sure its frames aren't included
 
 
     inVid = VideoFileClip("/tmp/raw.mp4") #raw input clip
@@ -30,15 +30,34 @@ def edit(times, key, s3client): #create and edit raw footage from speed change t
     for i in range(0, len(times)-1): #for all speed changes excluding the last
         if(times[i][1] != 0): #if not a pause
             if(i > 0 and times[i-1][1] == 0): #if a previous change exists and it is a pause, add a bit of an offset to remove any excess pause frames
-                clipstart = min(times[i][0]+unpauseOffset, inVid.duration)
+                prevPause = True
             else: #if previous change is not a pause or none exists
-                clipstart = min(times[i][0], inVid.duration)
+                prevPause = False
                 
             if(times[i+1][1] == 0): #if next speed change is a pause, cut earlier to remove any excess pause frames
-                clipend = max(times[i+1][0]+pauseOffset, 0)
+                nextPause = True
             else: #if next change is not a pause
+                nextPause = False
+            
+            if(prevPause and nextPause): #pause before and after current clip
+                clipstart = min(times[i][0]+unpauseOffset, times[i+1][0]-pauseOffset)
+                clipend = max(times[i+1][0]-pauseOffset, times[i][0]+unpauseOffset) 
+            elif(prevPause): #pause only before current clip
+                clipstart = min(times[i][0]+unpauseOffset, times[i+1][0]) 
+                clipend = times[i+1][0]
+            elif(nextPause): #pause only after current clip
+                clipstart = times[i][0]
+                clipend = max(times[i+1][0]-pauseOffset, times[i][0])
+            else: #no pause before or after current clip
+                clipstart = times[i][0]
                 clipend = times[i+1][0]
                 
+            clipstart = max(clipstart, 0)
+            clipend = max(clipend, 0)
+                
+            clipstart = min(clipstart, inVid.duration)
+            clipend = min(clipend, inVid.duration)
+            
             clip = inVid.subclip(clipstart, clipend)
             clip = clip.fx(vfx.speedx, 1/times[i][1])
             clips.append(clip)
